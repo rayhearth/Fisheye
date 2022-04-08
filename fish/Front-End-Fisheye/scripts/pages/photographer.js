58ff230880fc14bb5f5d7f0f624d9b6c34f4064c
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-04-08 18:00:56 +0200

    correction html et travail factory

--- fish/Front-End-Fisheye/scripts/pages/photographer.js
+++ fish/Front-End-Fisheye/scripts/pages/photographer.js
@@ -1,12 +1,13 @@
 import { Api } from '../api/_api.js'
-import { PhotographerPageCards, PhotographersIndexCards } from '../models/_photographersCards.js';
-
+import { PhotographFactory } from '../factories'
 
 //recherche du photographe via son id 
 const urlParams = new URLSearchParams(window.location.search)
 //on appelle la methode get de Api et on recupère 'id'
 const urlId = urlParams.get('id')
 
+let photograph = new PhotographFactory()
+// let all = await photograph.displayProfile(id)
 
 /*méthode pour recupérer tous les photographes*/
 async function getAllPhotograph() {
