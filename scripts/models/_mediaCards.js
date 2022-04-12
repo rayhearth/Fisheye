7deef1a1def810adbf6ae82fd81a8c327fb42a33
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-04-12 18:19:18 +0200

    correction mediacard lightbox

--- scripts/models/_mediaCards.js
+++ scripts/models/_mediaCards.js
@@ -21,17 +21,23 @@ export class MediaCards {
     renderLightbox(){
         if(this.type === 'image') {
             return `
-            <img class="picture" src="assets/photographers/${this._photographerId}/media/${this._media}" alt="${this._title}">`
+            <div class="lightbox-media-container" id=${this._id}>
+                <img class="picture" src="assets/photographers/${this._photographerId}/media/${this._media}" alt="${this._title}">
+                <h2 id="lightbox-media-title" class="lightbox-media-title" data-lightbox-caption>${this._title}</h2>
+            </div>`
         } else {
             return `
-            <video controls class="picture" media-id=${this._id} aria-label=${this._title}>
+            <div class="lightbox-media-container" id=${this._id}>
+                <video controls class="picture" media-id=${this._id} aria-label=${this._title}>
                 <source src="assets/photographers/${this._photographerId}/media/${this._media}"type="video/mp4"></video>
                 <svg class="svg-inline--fa fa-video mediaIcon" aria-hidden="true" focusable="false" data-prefix="fas"
                 data-icon="video" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg="">
                 <path fill="currentColor"
                 d="M384 112v288c0 26.51-21.49 48-48 48h-288c-26.51 0-48-21.49-48-48v-288c0-26.51 21.49-48 48-48h288C362.5 64 384 85.49 384 112zM576 127.5v256.9c0 25.5-29.17 40.39-50.39 25.79L416 334.7V177.3l109.6-75.56C546.9 87.13 576 102.1 576 127.5z">
                 </path>
-                </svg>`
+                </svg>
+                <h2 id="lightbox-media-title" class="lightbox-media-title" data-lightbox-caption>${this._title}</h2>
+            </div>`
         }
     }
     renderMedia() {
--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,8 +1,11 @@
 // DOM Elements
 let openLightboxBtn = document.querySelector('.mediaLink')
-let lightbox = document.querySelector("#lightbox");
 let closeLightboxBtn = document.querySelector('#lightbox__close')
-let mediaInLightbox = document.getElementById("lightbox-container");
+
+let mediaInLightbox = document.("lightbox-container")
+let lightboxTitle = document.querySelector('#lightbox-media-title')
+
+let lightbox = document.querySelector("#lightbox")
 let prevBtn = document.querySelector('#lightbox__prev')
 let nextBtn = document.querySelector('#lightbox__next')
 
