fdf04f71cc43c3481e2f011a964c9838a05c6da8
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-04-13 11:04:59 +0200

    branchement listener lightbox

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -2,7 +2,7 @@
 let openLightboxBtn = document.querySelector('.mediaLink')
 let closeLightboxBtn = document.querySelector('#lightbox__close')
 
-let mediaInLightbox = document.("lightbox-container")
+let mediaInLightbox = document.querySelector("lightbox-container")
 let lightboxTitle = document.querySelector('#lightbox-media-title')
 
 let lightbox = document.querySelector("#lightbox")
@@ -12,8 +12,25 @@ let nextBtn = document.querySelector('#lightbox__next')
 
 
 const openLightbox = () =>{
-
+    document.body.classList.add('overflow')
+    document.body.classList.add('lightboxOpen')
+    window.scrollTo(0, 0)
+    lightbox-media.scrollTo(0, 0)
+    
+    lightbox.setAttribute('aria-hidden', 'false')
+    lightbox.style.display = ''
 }
+
+//Fermeture de la lightbox
+const closeLightbox = () => {
+    document.body.classList.remove('lightboxOpen')
+    document.body.classList.remove('overflow')
+    document.removeEventListener('Keyup', this.onKeyUp)
+    document.querySelector('#openModal').focus()
+    lightbox.setAttribute('aria-hidden', 'true')
+
+    lightbox.style.display = 'none'
+  }
 
 let startlightboxlistener = () => {
     all = document.querySelectorAll('.mediaLink')
@@ -22,3 +39,5 @@ let startlightboxlistener = () => {
         m.addEventListener('click', openLightbox)
     }
 }
+
+closeLightboxBtn.addEventListener('click', closeLightbox)
