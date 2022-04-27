80468e644d5f16a5e3e67d5e0a70a5d80f0994f9
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-04-27 19:50:37 +0200

    travail filtre

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -41,62 +41,6 @@ let displayAllMedia = async () => {
     //appel de la methode des likes
     media.cuntMediaLike()
 
-    let filterBtns = document.querySelectorAll('.filterOption')
-    // console.log(filterBtns)
-    let mediasFiltered = [...document.querySelectorAll('.mediaLink')]
-    // console.log(mediasFiltered)
-
-    
-    
-    
-    
-    for (let btn of filterBtns) {
-        console.log(btn)
-        btn.addEventListener('click', e => {
-            mediaSort(e)
-        })
-    }
-    
-    const mediaSort = (e) => {
-        switch (e.target.id) {
-            case 'pop' : mediasFiltered.sort(function (a,b){
-                console.log('azeaze')
-                return a.dataset.cunt.LocaleCompare(b.dataset.cunt)
-            })
-            break
-        }
-        switch(e.target.id){
-            case 'date' : mediasFiltered.sort(function(a,b){
-                return  a.dataset.date.LocaleCompare(b.dataset.dataset)
-            })
-            break
-        }
-        document.querySelector('#galleryContainer').innerHTML = mediasFiltered.map(f => f.gallery)
-    }
-    // mediasFiltered.addEventListener("change", (e) => {
-    //     switch (e.target.id) {
-    //         case 'pop':
-    //             mediasFiltered.sort(function (a, b) {
-    //                 return a.dataset.cuntMediaLike.localeCompare(b.dataset.cuntMediaLike)
-    //             })
-    //             break
-    //     }
-
-    // filterBtns.forEach(filter => {
-    //     filter.addEventListener('change', (e) => {
-    //         switch (e.target.id) {
-    //             case 'pop':
-    //                 mediasFiltered.sort(function (a, b) {
-    //                     return a.dataset.cuntMediaLike.localeCompare(b.dataset.cuntMediaLike)
-    //                 })
-    //                 break
-    //         }
-    //     })
-    // displayAllMedia()
-    // console.log(e.target.id)
-    // })
-
-
-    
+    startDropboxListener()
 }
 window.addEventListener('load', displayAllMedia())
